3a9b15cec7569c1b80c377711e533b5cb01187ab
pablopi <pablopizarroaguilar@gmail.com>
2016-03-01 16:05:51 +0100

    Fixed URL format

--- src/resources.js
+++ src/resources.js
@@ -1,32 +1,16 @@
-/*
-We use this file to discover the devices.
-Once we find a device, we shuold:
-1. Register the device in the database, by following the given structure
-2. If the device is not available anymore, we should delete it from the database
-*/
-
 var request = require('request')
 // Sonos declaration
 var Sonos = require('sonos')
 
-/*
-We declare this variable in order to save the whole status of Sono devices.
-We will need that on routes.js
-*/
 var devices = []
 
 module.exports = function (callback) {
-  // First we ask the database for all the devices of the same brand that are stored
-  // We stored the hooks of this devices in the 'objects' array
   var objects = []
   // Request to the database
-  request.get(process.env.NETBEAST + '/api/resources?app=sonos',
+  request.get('http://' + process.env.NETBEAST + '/api/resources?app=sonos',
   function (err, resp, body) {
     if (err) return callback(err, null)
     if (!body) return callback()
-
-    body = JSON.parse(body)
-
     // Store the found devices in 'objects' array
     if (body.length > 0) {
       body.forEach(function (device) {
@@ -38,25 +22,17 @@ module.exports = function (callback) {
   // Implement the device discovery method
   Sonos.search(function (device) {
     devices.push(device)
-    // When we find a device
-    //  1. look if its already exists on the database.
-    var indx = objects.indexOf('/sonos/' + device.host) // hook == /Namebrand/id
-    //  2. If the hook is in 'objects' array, delete it from the array
+    var indx = objects.indexOf('/sonos/' + device.host)
     if (indx >= 0) {
       objects.splice(indx, 1)
-      // 3. If this device is not registered on the database, you should register it
     } else {
-      //  Use this block to register the found device on the netbeast database
-      //  in order to using it later
-      request.post({url: process.env.NETBEAST + '/api/resources',
+      request.post({url: 'http://' + process.env.NETBEAST + '/api/resources',
       json: {
-        app: 'sonos',          // Name of the device brand
+        app: 'sonos',
         location: 'none',
-        topic: 'music',      // lights, bridge, switch, temperature, sounds, etc
+        topic: 'music',
         groupname: 'none',
-        hook: '/sonos/' + device.host  // hook == /Namebrand/id
-        // We will use the id to access to the device and modify it.
-        // Any value to refer this device (MacAddress, for example) can work as id
+        hook: '/sonos/' + device.host
       }},
       function (err, resp, body) {
         if (err) return callback(err, null)
@@ -64,9 +40,6 @@ module.exports = function (callback) {
       })
     }
   })
-  // If there exists 'hooks' stored on the 'objects' array, it means that
-  // this devices are stored on the databases but are not reachable. We should
-  // delete this devices from the database using the code given.
 
   setTimeout(function () {
     if (objects.length > 0) {
@@ -79,12 +52,6 @@ module.exports = function (callback) {
       })
     }
 
-    /*
-    This function is called from route.js, so
-    if you need to pass any information to this file you can do it
-    by using the callback ( callback(error, devices) , for example)
-    */
     return callback(null, devices)
-    devices = []
   }, 7000)
 }
